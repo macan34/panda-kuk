5db40fdab4f9ab0fbd0c8a568720f7e61a96c7cc
macan34 <macangoblok1@gmail.com>
2025-11-15 15:08:27 +0700

    Add GitHub Pages auto deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/panda-kuk/', // WAJIB untuk GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
